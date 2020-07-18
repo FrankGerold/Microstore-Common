a88de231d64c8aa1e755e52ece418582a1033ccb
Frank Gerold <FrankETSGerold@gmail.com>
2020-07-18 10:20:22 -0400

    Moved all Errors and Middleware into common repo, exporting them.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
-interface Color {
-  red: number;
-  green: number;
-  blue: number;
-};
-
-const color: Color = {
-  red: 244,
-  blue: 156,
-  green: 142
-};
-
-console.log(color);
-
-export default color;
+export * from './errors';
+export * from './middleware';
